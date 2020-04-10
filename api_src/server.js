d33b70cc206ea9f56a0c561c660bb5e84b87c43d
Ilie Cebanu <cebanu5665@gmail.com>
2020-04-10 16:21:07 +0100

    :fire: deleted commented out code

--- api_src/server.js
+++ api_src/server.js
@@ -3,10 +3,7 @@ import express from "express";
 import store from './store/store';
 import uploadRouter from "./endpoints/upload";
 
-import { actions as userUploadActions } from "../shared/store/userUpload";
 import { actions as sharedStateActions } from "../shared/store/sharedState";
-
-import { Worker } from 'worker_threads';
 
 const app = express();
 
@@ -34,29 +31,6 @@ setTimeout(() => {
         }
     }));
 }, 1000);
-
-const asyncWrappedWorker = (data, path) => new Promise((resolve, reject) => {
-    const worker = new Worker(path);
-    worker.once('message', result => {
-        resolve(result);
-    });
-    worker.once('error', error => {
-        reject(error);
-    });
-    worker.postMessage(data);
-});
-
-// asyncWrappedWorker(68, `${__dirname}/plusOneWorker.js`)
-//     .then(data => console.log(data))
-//     .catch(error => console.error(error));
-
-// setTimeout(() => {
-//     for (let i = 0; i < 10; i++) {
-//         store.dispatch(userUploadActions.test("dingus amingus"))
-//         store.dispatch(userUploadActions.test("dingus amingus"))
-//     }
-// }, 5000);
-// console.log("testing my logs lmao")
 
 app.get("/", (req, res) => {
     res.send(store.getState());
