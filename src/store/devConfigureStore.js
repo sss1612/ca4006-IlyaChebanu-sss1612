242a2dd97dda3816d7ae8131e6b2d32dafaa4a21
Ilie Cebanu <cebanu5665@gmail.com>
2020-04-11 06:07:41 +0100

    :zap: sanitized devtools

--- src/store/devConfigureStore.js
+++ src/store/devConfigureStore.js
@@ -6,7 +6,20 @@ const devConfigureStore = sagaMiddleware => createStore(
     rootReducer,
     compose(
         applyMiddleware(sagaMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({
+            actionSanitizer: action => (
+                ['sharedState/ADD_METADATA', 'sharedState/SET_INITIAL_STATE'].includes(action.type)
+                    ? { type: action.type, data: 'tl;dr' }
+                    : action
+            ),
+            stateSanitizer: state => ({
+                ...state,
+                sharedState: {
+                    ...state.sharedState,
+                    metaData: 'tl;dr'
+                }
+            })
+        }),
     )
 )
 
