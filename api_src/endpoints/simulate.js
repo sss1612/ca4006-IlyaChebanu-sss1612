130dddd06018cb79e9cb89ee6c06640293a80624
Ilie Cebanu <cebanu5665@gmail.com>
2020-04-14 04:31:01 +0100

    remove logs

--- api_src/endpoints/simulate.js
+++ api_src/endpoints/simulate.js
@@ -8,9 +8,8 @@ const router = express.Router()
 
 router.post("/simulate",  (req, res) => {
     const { flag } = req.body;
-    console.log(" A BOOOOOOOOOOL", flag)
     store.dispatch(sharedStateActions.simulateForcedDiskSpace(flag));
-    res.send(204)
+    res.sendStatus(204)
 })
 
 export default router;
